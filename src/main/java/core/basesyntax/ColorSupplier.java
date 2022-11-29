fd90b9f0b37236522eb4e32f3c3ee1b5ae2a5364
Leshchenko <411100253g@gmail.com>
2022-11-29 13:43:08 +0200

    Leshchenko solution 2022-11-29  - added class Ball  - added class Lottery  - added enum ColorEnum  - implemented getRandomColor method in ColorSupplier class  - changed Main method in Application class

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        ColorEnum[] values = ColorEnum.values();
+        int length = values.length;
+        int randIndex = new Random().nextInt(length);
+        return values[randIndex].name();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+
+    public Ball getRandomBall() {
+        return new Ball(new Random().nextInt(MAX_BALL_NUMBER),
+                        new ColorSupplier().getRandomColor()
+        );
+    }
+}
